43b91518b5fd86a59da97a85f8ca599b95c38a42
Jeff Swisher <jefftswisher@gmail.com>
2019-10-20 18:26:14 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,11 @@
 class Book
   
-  attr_accessor(:title, :author, :page_count,  )
-  def initialize()
+  attr_accessor(:title, :author, :page_count, :genre)
+  
+  def initialize(title)
+    @title = title
+  end 
+  
+  def 
 end 
 
