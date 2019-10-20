5afddac364206c7539ca248d9316308e9174beee
Jeff Swisher <jefftswisher@gmail.com>
2019-10-20 18:40:10 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,15 +1,13 @@
 class Shoe 
-attr_accessor(:color, :size, :material, :condition)
-attr_reader(:brand)
-
-def initialize(brand)
-  @brand = brand 
-end 
-
-def cobble
-   puts "Your shoe is as good as new!"
-end 
-
- 
-
+  attr_accessor(:color, :size, :material, :condition)
+  attr_reader(:brand)
+  
+  def initialize(brand)
+    @brand = brand 
+  end 
+  
+  def cobble
+     puts "Your shoe is as good as new!"
+     shoe.condition = "new"
+  end 
 end 
